68db317e7b632b2123308cfd1343710a5d8a2b1d
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-12-05 05:00:10 +0800

    ui

--- Assets/Scripts/Managers/SelectionManager.cs
+++ Assets/Scripts/Managers/SelectionManager.cs
@@ -12,7 +12,7 @@ public class SelectionManager : MonoBehaviour
     public static SelectionManager instance { get; private set; } = null;
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
             instance = this;
         else Destroy(gameObject);
     }
@@ -22,17 +22,27 @@ public class SelectionManager : MonoBehaviour
     [SerializeField] private GameObject validSelectionIndicator;
     [SerializeField] private float yOffset = 1;
 
-    private List<Unit> GetValidTargets(int PlayerIndex, Unit Requester, ETargetType TargetingSchema)
+
+    public Ability selectionSubject = null;
+    private Unit requester;
+
+    private List<GameObject> indicators = new();
+    List<Unit> validSelectables = new();
+
+
+    public List<Unit> selection;
+
+    private List<Unit> GetValidTargets(int PlayerIndex, ETargetType TargetingSchema)
     {
         var PlayerUnits = PlayerIndex == 1 ? BoardStateManager.instance.PlayerAUnit : BoardStateManager.instance.PlayerBUnit;
         var EnemyUnits = PlayerIndex == 1 ? BoardStateManager.instance.PlayerBUnit : BoardStateManager.instance.PlayerAUnit;
 
         switch (TargetingSchema)
         {
-            case ETargetType.SELF: return new() { Requester };
+            case ETargetType.SELF: return new() { requester };
             case ETargetType.ALLY:
             case ETargetType.ALL_ALLIES: return PlayerUnits;
-            case ETargetType.OTHER_ALLIES: return PlayerUnits.Where(t => t != Requester).ToList();
+            case ETargetType.OTHER_ALLIES: return PlayerUnits.Where(t => t != requester).ToList();
             case ETargetType.ENEMY_SINGLE:
             case ETargetType.ALL_ENEMIES: return EnemyUnits;
         }
@@ -40,22 +50,64 @@ public class SelectionManager : MonoBehaviour
         return null;
     }
 
-
-    public async Task<List<Unit>> MakeSelection(int PlayerIndex, Unit Requester, ETargetType TargetingSchema, CancellationToken cancel)
+    public void RequestSelection(Ability subject, int PlayerIndex, Unit Requester)
     {
-        List<Unit> validTargets = GetValidTargets(PlayerIndex, Requester, TargetingSchema);
+        if (subject != null)
+            return;
 
-        foreach (var target in validTargets)
+        selectionSubject = subject;
+        requester = Requester;
+
+        validSelectables = GetValidTargets(PlayerIndex, subject.TargetType);
+
+        foreach (Unit validSelectable in validSelectables)
         {
             GameObject indicator = Instantiate(validSelectionIndicator);
-            indicator.transform.parent = target.FieldObject.transform;
+            indicator.transform.parent = validSelectable.FieldObject.transform;
             indicator.transform.localPosition = Vector3.up * yOffset;
+            indicators.Add(indicator);
         }
 
-        foreach (var target in validTargets)
-            Destroy(target.FieldObject);
-
-            return null;
+   
+        //subscribe event here
     }
 
+    public void OnTap(Dictionary <string, object> p)
+    {
+        Unit unitSelected = BoardStateManager.instance.ModelLookUp(p["Model"] as GameObject);
+
+        if (unitSelected == null || !validSelectables.Contains(unitSelected))
+            return;
+
+        selection = new();
+
+        /*
+        switch (selectionSubject.TargetType)
+        {
+            case ETargetType.SELF: selection.Add(requester); break;
+            case ETargetType.ALLY: 
+            case ETargetType.ENEMY_SINGLE: selectionSubject.
+
+
+            case ETargetType.ALL_ALLIES: 
+            case ETargetType.OTHER_ALLIES:
+  
+            case ETargetType.ALL_ENEMIES: return EnemyUnits;
+        }
+
+
+        AbortSelection();
+
+        //fire event here
+        */
+    }
+
+    private void AbortSelection()
+    {
+        selectionSubject = null;
+        foreach (var indicator in indicators)
+            Destroy(indicator);
+        indicators.Clear();
+        //fire event here
+    }
 }
